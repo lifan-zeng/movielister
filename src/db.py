6a72c58c076f038da73a82d5357204666d06f440
Lifan Zeng <lifanzeng000@gmail.com>
2020-05-14 03:02:29 -0700

    fixed problems with table relationships

--- src/db.py
+++ src/db.py
@@ -9,11 +9,11 @@ import time
 
 db = SQLAlchemy()
 
-#association table between User and WatchedList
-user_movie_list_table = db.Table('user_movie_list', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('movie_list_id', db.Integer, db.ForeignKey('movie_list.id'))  
-)
+# #association table between User and WatchedList
+# user_movie_list_table = db.Table('user_movie_list', db.Model.metadata,
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('movie_list_id', db.Integer, db.ForeignKey('movie_list.id'))  
+# )
 
 #association table between MovieList and Movie
 movie_in_list_table = db.Table('movie_in_list', db.Model.metadata,
@@ -37,12 +37,10 @@ class User(db.Model):
     movie_lists = db.relationship('MovieList', cascade='delete')
     friends = db.relationship('User',
                     secondary = friend_table,
-                    primaryjoin = id==friend_table.c.user_id_1,
-                    secondaryjoin = id==friend_table.c.user_id_2,
-                    backref='user_id_2'
-    )
-
-    #add relationships
+                    primaryjoin = (id==friend_table.c.user_id_1),
+                    secondaryjoin = (id==friend_table.c.user_id_2),
+                    backref=db.backref('friend'),
+                    remote_side=[id])
 
     def __init__(self, **kwargs):
         self.username = kwargs.get('username', '')
@@ -55,8 +53,20 @@ class User(db.Model):
             'username': self.username,
             'email': self.email,
             'time_created': self.time_created,
-            'movie_lists': [ml.serialize() for ml in self.movie_lists]
+            'movie_lists': [ml.serialize() for ml in self.movie_lists],
+            'friends': self.serialize_friends()
         }
+    
+    def serialize_friends(self):
+        friends_serialized = []
+        for f in self.friends:
+            serial = {
+                'id': f.id,
+                'username': f.username
+            }
+            friends_serialized.append(serial)
+        return friends_serialized
+    
 
 
 class MovieList(db.Model):
@@ -66,20 +76,7 @@ class MovieList(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     # user = db.relationship('User', secondary=user_movie_list_table, back_populates='movie_lists')
     
-
-    # watched_list = db.relationship('WatchedList', back_populates='movie_list', cascade='delete') 
-    # plan_to_watch_list = db.relationship('PlanToWatchList', back_populates='movie_list', cascade='delete') 
-    # play_list = db.relationship('PlayList', back_populates='movie_list', cascade='delete') 
-
     movies = db.relationship('Movie', secondary=movie_in_list_table, back_populates='lists')
-
-    #add relationships
-
-    # list_type = db.Column(db.String)
-    # __mapper_args__ = {
-    #     'polymorphic_identity':'list',
-    #     'polymorphic_on': list_type
-    # }
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
@@ -88,73 +85,12 @@ class MovieList(db.Model):
     def serialize(self, show_movies=True):
         serial = {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'owner_id': self.user_id
         }
         if show_movies:
             serial['movies'] = [m.serialize() for m in self.movies]
         return serial
-
-
-# class WatchedList(db.Model):
-#     __tablename__ = 'watched_list'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     movie_list_id = db.Column(db.Integer, db.ForeignKey('movie_list.id'))
-#     movie_list = db.relationship('User', uselist=False, back_populates='watched_list')
-
-#     def __init__(self, **kwargs):
-#         self.movie_list_id = kwargs.get('movie_list_id')
-    
-#     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), back_populates='watched_list')
-    
-#     # def __init__(self, **kwargs):
-#     #     super().__init__(self, **kwargs)
-
-#     # __mapper_args__ = {
-#     #     'polymorphic_identity':'watched movie list',
-#     # }
-
-
-# class PlanToWatchList(MovieList):
-#     __tablename__ = 'plan_to_watch_list'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     movie_list_id = db.Column(db.Integer, db.ForeignKey('movie_list.id'))
-#     movie_list = db.relationship('User', uselist=False, back_populates='plan_to_watch_list')
-    
-#     def __init__(self, **kwargs):
-#         self.movie_list_id = kwargs.get('movie_list_id')
-
-#     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), back_populates='plan_to_watch_list')
-    
-#     # def __init__(self, **kwargs):
-#     #     super().__init__(self, **kwargs)
-
-#     # __mapper_args__ = {
-#     #     'polymorphic_identity':'plan to watch movie list'
-#     # }
-
-
-# class PlayList(MovieList):
-#     __tablename__ = 'play_list'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     movie_list_id = db.Column(db.Integer, db.ForeignKey('movie_list.id'))
-#     movie_list = db.relationship('User', uselist=False, back_populates='play_list')
-
-#     def __init__(self, **kwargs):
-#         self.movie_list_id = kwargs.get('movie_list_id')
-
-    
-#     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), back_populates='play_list')
-
-#     # def __init__(self, **kwargs):
-#     #     super().__init__(self, **kwargs)
-
-#     # __mapper_args__ = {
-#     #     'polymorphic_identity':'play list'
-#     # }
-
 
 class Movie(db.Model):
     __tablename__ = 'movie'
