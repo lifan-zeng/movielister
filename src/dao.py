0354f9f7379c99c1469d445e6ddfbf60b54d6656
Lifan Zeng <lifanzeng000@gmail.com>
2020-05-14 03:01:17 -0700

    update movie list and friends methods

--- src/dao.py
+++ src/dao.py
@@ -10,16 +10,7 @@ def create_user(username, email):
     db.session.commit()
     new_watched_list = create_movie_list(name='Watched', user_id=new_user.id)
     new_plan_to_watch_list = create_movie_list(name='Plan to Watch', user_id=new_user.id)
-    # _create_default_lists(new_user.id)
     return new_user.serialize()
-
-def _create_default_lists(user_id):
-    print('entered default')
-    user = get_user_by_id(user_id)
-    if user is None:
-        return None
-    new_watched_list = create_movie_list(name='Watched', user_id=user_id)
-    new_plan_to_watch_list = create_movie_list(name='Plan to Watch', user_id=user_id)
 
 def get_user_by_id(user_id):
     user = User.query.filter_by(id=user_id).first()
@@ -48,13 +39,31 @@ def delete_user_by_id(user_id):
 def add_user_as_friend(user_id_1, user_id_2):
     user_1 = get_user_by_id(user_id_1)
     user_2 = get_user_by_id(user_id_2)
-    if (user_1 or user_2) is None:
+    if user_1 is None or user_2 is None:
         return None
-    user_1.friends.append(user_2)
-    user_2.friends.append(user_1)
-    return user_1.serialize()
-    
+    user_1_query = User.query.filter_by(id=user_id_1).first()
+    user_2_query = User.query.filter_by(id=user_id_2).first()
+    user_1_query.friends.append(user_2_query)
+    user_2_query.friends.append(user_1_query)
+    db.session.commit()
 
+    return user_1_query.serialize()
+
+def remove_user_as_friend(user_id_1, user_id_2):
+    user_1 = get_user_by_id(user_id_1)
+    user_2 = get_user_by_id(user_id_2)
+    if user_1 is None or user_2 is None:
+        return None
+    user_1_query = User.query.filter_by(id=user_id_1).first()
+    user_2_query = User.query.filter_by(id=user_id_2).first()
+    if user_2_query not in list(user_1_query.friends):
+        return None
+    user_1_query.friends.remove(user_2_query)
+    user_2_query.friends.remove(user_1_query)
+    db.session.commit()
+    return user_1_query.serialize()
+
+    return user_1_query.serialize()
 def create_movie_list(name, user_id):
     new_list = MovieList(name=name, user_id=user_id)
     user = get_user_by_id(user_id)
@@ -77,24 +86,35 @@ def get_movie_list_by_id(movie_list_id):
     return movie_list.serialize()
 
 def update_movie_list_by_id(movie_list_id, body):
-    movie_list = MovieList.query.filter_by(id=movie_list_id).first()
+    movie_list = get_movie_list_by_id(movie_list_id)
     if movie_list is None:
         return None
+    movie_list = MovieList.query.filter_by(id=movie_list_id).first()    
     movie_list.name = body.get('name', movie_list.name)
-    movies_to_add = body.get('movies', None)
+    movies_to_add = body.get('add_movies', None)
+    movies_to_remove = body.get('remove_movies', None)
     movies_not_added = []
+    movies_not_removed = []
     if movies_to_add is not None:
         for movie_id in movies_to_add:
             movie = get_movie_by_id(movie_id)
             if movie is None:
-                movies_not_added.append(movie_id)
-            movie_list.movies.append(list(movie))
+                # movies_not_added.append(movie_id)
+                return None
+            movie = Movie.query.filter_by(id=movie_id).first()
+            movie_list.movies.append(movie)
+    if movies_to_remove is not None:
+        for movie_id in movies_to_remove:
+            movie = get_movie_by_id(movie_id)
+            if movie is None:
+                # movies_not_removed.append(movie_id)
+                return None
+            movie = Movie.query.filter_by(id=movie_id).first()
+            if movie not in movie_list.movies:
+                return None
+            movie_list.movies.remove(movie)
     db.session.commit()
-    return movie_list.serialize()
-            
-    #ADD THE "ADD MOVIE" OPTION    
-
-    
+    return movie_list.serialize()  
 
 def delete_movie_list_by_id(movie_list_id):
     movie_list = MovieList.query.filter_by(id=movie_list_id).first()
@@ -111,13 +131,13 @@ def add_movie(title, director, year):
     return new_movie.serialize()
 
 def get_movie_by_id(movie_id):
-    movie = MovieList.query.filter_by(id=movie_id).first()
+    movie = Movie.query.filter_by(id=movie_id).first()
     if movie is None:
         return None
     return movie.serialize()
 
 def update_movie_by_id(movie_id, body):
-    movie = MovieList.query.filter_by(id=movie_id).first()
+    movie = Movie.query.filter_by(id=movie_id).first()
     if movie is None:
         return None
     movie.title = body.get('title', movie.title)
@@ -127,7 +147,7 @@ def update_movie_by_id(movie_id, body):
     return movie.serialize()
 
 def delete_movie_by_id(movie_id):
-    movie = MovieList.query.filter_by(id=movie_id).first()
+    movie = Movie.query.filter_by(id=movie_id).first()
     if movie is None:
         return None
     db.session.delete(movie)
